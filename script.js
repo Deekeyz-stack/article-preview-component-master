558431d81d1390c98da1b564d2ebb15aaf0d266c
Deekeyz-stack <davo4success@gmail.com>
2020-11-28 23:13:46 +0000

    initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+const btn = document.querySelector('.menu');
+const footer = document.querySelector('#footer');
+
+btn.addEventListener('click', function (){
+    footer.classList.toggle('show');
+});
+
+
